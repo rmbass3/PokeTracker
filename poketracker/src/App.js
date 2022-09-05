63e759cef68c3e15bcf08adbc75b338943e9c058
Ryan <rmbass3@gmail.com>
2022-09-05 12:00:06 -0400

    Setup env var and api request

--- poketracker/src/App.js
+++ poketracker/src/App.js
@@ -1,6 +1,20 @@
 import React from 'react';
 import Home from './components/Home';
 import './App.css';
+const { REACT_APP_PRIVATE_KEY } = process.env;
+
+//console.log(REACT_APP_PRIVATE_KEY)
+
+const axios = require('axios')
+
+axios.get('https://api.pokemontcg.io/v2/cards/base1-4', {
+    headers: {
+      "x-api-key": REACT_APP_PRIVATE_KEY,
+    }
+  })
+  .then(res => console.log(res))
+  .catch(err => console.log(err))
+
 
 function App() {
   return (
