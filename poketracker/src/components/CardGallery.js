41d6f8089541cf8b1fef1f2d85703f85f38f212b
Ryan <rmbass3@gmail.com>
2022-09-06 09:28:52 -0400

    Implemented react tilty

--- poketracker/src/components/CardGallery.js
+++ poketracker/src/components/CardGallery.js
@@ -1,7 +1,6 @@
 import React from "react";
-
 import Col from 'react-bootstrap/Col'
-
+import Tilty from 'react-tilty'
 
 const CardGallery = ({cards}) => {
 
@@ -9,9 +8,12 @@ const CardGallery = ({cards}) => {
 
   return cards.data.data.map((card) => (
     <Col>
-      <div key={card.id} className="card-container mt-5 text-center">
-        <img src={card.images.small} className="card-image" alt={card.name}/>
-      </div>
+      <Tilty
+      >
+        <div key={card.id} className="card-container mt-5 text-center">
+          <img src={card.images.small} className="card-image" alt={card.name}/>
+        </div>
+      </Tilty>
     </Col>
   ))
 
