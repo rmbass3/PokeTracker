95725b630b7bf9189436ff60132532b944554718
Ryan <rmbass3@gmail.com>
2022-09-07 08:16:53 -0400

    Created modal grid

--- poketracker/src/components/CardModal.js
+++ poketracker/src/components/CardModal.js
@@ -1,6 +1,10 @@
 import React from "react";
 import Modal from 'react-bootstrap/Modal';
 import Button from 'react-bootstrap/Button';
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Container from "react-bootstrap/Container";
+import '../styles/CardModal.css'
 
 const CardModal = ({show, handleClose, currCard}) => {
 
@@ -8,13 +12,30 @@ const CardModal = ({show, handleClose, currCard}) => {
   return (
     <div>
       {currCard.id ?
-            <div className="card-modal">
+          <div className="card-modal">
             <Modal show={show} onHide={handleClose} size="lg" centered>
               <Modal.Header closeButton>
                 <Modal.Title>{currCard.name}</Modal.Title>
               </Modal.Header>
               <Modal.Body>
-                <img src={currCard.images.large} alt={currCard.name}></img>
+                <Container>
+                  <Row>
+                    <Col xs={12} md={6}>
+                      <img 
+                        className="modal-img" 
+                        src={currCard.images.large} 
+                        alt={currCard.name}>
+                      </img>
+                    </Col>
+                    <Col xs={12} md={6}>
+                      <img 
+                        className="modal-img" 
+                        src={currCard.images.large} 
+                        alt={currCard.name}>
+                      </img>
+                    </Col>
+                  </Row>
+                </Container>
               </Modal.Body>
               <Modal.Footer>
                 <Button variant="secondary" onClick={handleClose}>
