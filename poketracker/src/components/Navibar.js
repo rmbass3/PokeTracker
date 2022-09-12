903df27160079ba48bbacd3fc8aaebc823e939e4
Ryan <rmbass3@gmail.com>
2022-09-12 12:47:49 -0400

    Added links between pages

--- poketracker/src/components/Navibar.js
+++ poketracker/src/components/Navibar.js
@@ -4,6 +4,7 @@ import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import { Link } from "react-router-dom";
 import '../styles/Navibar.css'
 
 const Navibar = ({searchBar, setSearchBar, setSearch}) => {
@@ -17,13 +18,13 @@ const Navibar = ({searchBar, setSearchBar, setSearch}) => {
   return (
     <Navbar bg="dark" variant="dark" expand="lg">
       <Container fluid>
-        <Navbar.Brand href="#home">PokéTracker</Navbar.Brand>
+        <Navbar.Brand><Link to="/" className="text-light">PokéTracker</Link></Navbar.Brand>
         <Navbar.Toggle aria-controls="navbarScroll" />
         <Navbar.Collapse id="navbarScroll">
           <Nav
             className="me-auto my-2 my-lg-0"
           >
-            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link><Link to="/login" className="text-light">Login</Link></Nav.Link>
             <Nav.Link href="#about">About</Nav.Link>
             <Nav.Link href="#collection">Collection</Nav.Link>
             <Nav.Link href="#adv-search">Advanced Search</Nav.Link>
