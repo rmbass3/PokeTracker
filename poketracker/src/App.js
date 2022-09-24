cad785f79ee1d91533d36d43f8803b17dbf5f82e
Ryan <rmbass3@gmail.com>
2022-09-24 16:31:19 -0400

    Added additional loading checks

--- poketracker/src/App.js
+++ poketracker/src/App.js
@@ -17,12 +17,12 @@ function App() {
   const [cards, setCards] = useState({})
   const [search, setSearch] = useState("charizard")
   const [searchBar, setSearchBar] = useState("")
-  const [isLoaded, setIsLoaded] = useState(false)
+  const [cardsLoaded, setCardsLoaded] = useState(false)
   const [user, loading] = useAuthState(auth)
   const navigate = useNavigate()
 
   const searchCards = () => {
-    setIsLoaded(false)
+    setCardsLoaded(false)
     axios.get('https://api.pokemontcg.io/v2/cards', {
       headers: {
         "x-api-key": REACT_APP_PRIVATE_KEY,
@@ -32,7 +32,7 @@ function App() {
       }
     })
     .then(res => {
-      setIsLoaded(true)
+      setCardsLoaded(true)
       setCards(res)
     })
     .catch(err => console.log(err))
@@ -43,10 +43,12 @@ function App() {
   }, [search])
 
   useEffect(() => {
-    if (user) {
-      navigate("/")
-    } else {
-      navigate("/login")
+    if (!loading){
+      if (user) {
+        navigate("/")
+      } else {
+        navigate("/login")
+      }
     }
   }, [user, loading])
 
@@ -54,7 +56,7 @@ function App() {
     <div className="App">
       {user ? <Navibar searchBar={searchBar} setSearchBar={setSearchBar} setSearch={setSearch} user={user}/> : <></>}
       <Routes>
-        <Route path="/" element={<Home cards={cards} isLoaded={isLoaded} setIsLoaded={setIsLoaded} user={user}/>} />
+        <Route path="/" element={<Home cards={cards} cardsLoaded={cardsLoaded} setCardsLoaded={setCardsLoaded} loadingUser={loading} user={user}/>} />
         <Route path="/login" element={<Login/>} />
         <Route path="/reset" element={<Reset/>} />
         <Route path="/register" element={<Register/>} />
