232da6800f568c861277e6146359aa8c68758840
Ryan <rmbass3@gmail.com>
2022-09-24 17:12:29 -0400

    Added user icon for navbar

--- poketracker/src/components/Navibar.js
+++ poketracker/src/components/Navibar.js
@@ -7,6 +7,8 @@ import Navbar from 'react-bootstrap/Navbar';
 import { Link } from "react-router-dom";
 import { useNavigate } from 'react-router-dom';
 import { logout } from "../Firebase";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUser } from '@fortawesome/free-solid-svg-icons'
 import '../styles/Navibar.css'
 
 
@@ -26,37 +28,36 @@ const Navibar = ({searchBar, setSearchBar, setSearch, user}) => {
     navigate("/login")
   }
 
+  console.log(user.photoURL)
+
 
   return (
     <Navbar bg="dark" variant="dark" expand="lg">
       <Container fluid>
-        <Navbar.Brand><Link to="/" className="text-light">PokéTracker</Link></Navbar.Brand>
+        <Navbar.Brand className="ms-1"><Link to="/" className="text-light">PokéTracker</Link></Navbar.Brand>
         <Navbar.Toggle aria-controls="navbarScroll" />
         <Navbar.Collapse id="navbarScroll">
           <Nav
             className="me-auto my-2 my-lg-0"
           >
-            <Navbar.Text onClick={handleLogout}><Link to="/login" className="text-light">Logout</Link></Navbar.Text>
+            {/* <Navbar.Text onClick={handleLogout}><Link to="/login" className="text-light">Logout</Link></Navbar.Text>
             <Navbar.Text href="#about">About</Navbar.Text>
             <Navbar.Text href="#collection">Collection</Navbar.Text>
-            <Navbar.Text href="#adv-search">Advanced Search</Navbar.Text>
+            <Navbar.Text href="#adv-search">Advanced Search</Navbar.Text> */}
+            <Form className="d-flex" onSubmit={handleSubmit}>
+              <Form.Control
+                type="search"
+                placeholder="Search"
+                className="me-2"
+                aria-label="Search"
+                value={searchBar}
+                onChange={e => setSearchBar(e.target.value)}
+              />
+            </Form>
           </Nav>
-          <Form className="d-flex" onSubmit={handleSubmit}>
-            <Form.Control
-              type="search"
-              placeholder="Search"
-              className="me-2"
-              aria-label="Search"
-              value={searchBar}
-              onChange={e => setSearchBar(e.target.value)}
-            />
-            <Button 
-              type="submit"
-              variant="outline-secondary"
-            >
-              Search
-            </Button>
-          </Form>
+          <Nav>
+            <FontAwesomeIcon color="#fff" icon={faUser} border/>
+          </Nav>
         </Navbar.Collapse>
       </Container>
     </Navbar>
